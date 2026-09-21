5feab48329db57a8896725190f30d06657a74ddb
jackkurian044-maker <jackkurian044@gmail.com>
2026-09-21 14:20:46 +0530

    Add dedicated business page boot

---
+++ src/public-business-entry.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+import PublicBusinessPage from "./PublicBusinessPage.jsx";
+
+const match = window.location.pathname.match(/^\/business\/([^/]+)\/?$/i);
+const listingId = match ? match[1] : "";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PublicBusinessPage listingId={listingId} />
+  </React.StrictMode>
+);
